e5903fc204b547df9808bf16927bd51e5cc6a80a
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-04-20 00:20:04 +0000

    change the port

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const app = require('./app')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
-dotenv.config()
-const port = process.env.PORT || 9000
-connectDB()
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+dotenv.config();
+const port = process.env.PORT || 8000;
+connectDB();
 
-
-app.listen(port, () => console.log(`server running on port ${port}...`))
+app.listen(port, () => console.log(`server running on port ${port}...`));
