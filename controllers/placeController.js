01017872ef6007f6f4f396765a09359cd2bd3c61
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-04-22 11:25:38 +0000

    finish the error handling stragtegy

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -1,118 +1,118 @@
-"use strict"
-const Place = require('./../models/placeModel')
-const APIFeatures = require('./../utils/APIFeatures')
+"use strict";
+const Place = require("./../models/placeModel");
+const APIFeatures = require("./../utils/APIFeatures");
+const catchAsync = require("./../utils/catchAsync");
 
-const multer = require('multer')
-
+const multer = require("multer");
+const AppError = require("../utils/appError");
 
 const multerStorage = multer.diskStorage({
- destination: (req, file, cb) =>
- {
-  cb(null, 'public/img/places')
- },
- filename: (req, file, cb) =>
- {
-  const ext = file.mimetype.split('/').at(-1)
-  cb(null, `place-${Date.now()}-${Math.random() * 1000}.${ext}`)
- }
-})
+  destination: (req, file, cb) => {
+    cb(null, "public/img/places");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/").at(-1);
+    cb(null, `place-${Date.now()}-${Math.random() * 1000}.${ext}`);
+  },
+});
 
-const multerFilter = (req, file, cb) =>
-{
- if (file.mimetype.startsWith('image')) {
-  cb(null, true)
- } else {
-  cb(new Error('file is not an image'), false)
- }
-}
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("file is not an image"), false);
+  }
+};
 
 const upload = multer({
- storage: multerStorage,
- fileFilter: multerFilter
-})
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
-const uploadPlaceImages = upload.array('images', 15)
+const uploadPlaceImages = upload.array("images", 15);
 
-const getAllPlaces = async (req, res) =>
-{
- try {
-  const features = new APIFeatures(Place.find(), req.query)
-   .filter()
-   .sort()
-   .limitFields()
-   .paginate()
-  const places = await features.query
+const getAllPlaces = async (req, res) => {
+  try {
+    const features = new APIFeatures(Place.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const places = await features.query;
+    res.status(200).json({
+      status: "success",
+      result: places.length,
+      body: {
+        places,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      // reason: "something went wrong"
+      reason: err,
+    });
+  }
+};
+
+const getOnePlace = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const place = await Place.findById(id);
+  if (!place) {
+    return next(new AppError(`no place found with this id : ${id}`, 404));
+  }
   res.status(200).json({
-   status: "success",
-   result: places.length,
-   body: {
-    places
-   }
-  })
- } catch (err) {
-  res.status(404).json({
-   status: "fail",
-   // reason: "something went wrong"
-   reason: err
-  })
- }
-}
+    status: "success",
+    result: place.length,
+    body: {
+      place,
+    },
+  });
+});
 
-const getOnePlace = async (req, res) =>
-{
- try {
-  const place = await Place.findById(req.params.id)
-  res.status(200).json({
-   status: "success",
-   result: place.length,
-   body: {
-    place
-   }
-  })
- } catch (err) {
-  res.status(404).json({
-   status: "fail",
-   reason: "something went wrong"
-  })
- }
-}
-
-const createPlace = async (req, res) =>
-{
- try {
-  const { title, location, host, price, description, category, from, to, maxAdults, maxChildren, maxInfants,
-   maxPets } = req.body
-  const images = req.files?.map(image => `http://localhost:${process.env.PORT}/img/places/${image.filename}`)
-  if (!title || !location || !host || !price || !images) throw new Error('something went wrong !')
+const createPlace = catchAsync(async (req, res, next) => {
+  const {
+    title,
+    location,
+    host,
+    price,
+    description,
+    category,
+    from,
+    to,
+    maxAdults,
+    maxChildren,
+    maxInfants,
+    maxPets,
+  } = req.body;
+  const images = req.files?.map(
+    (image) =>
+      `http://localhost:${process.env.PORT}/img/places/${image.filename}`
+  );
+  // if (!title || !location || !host || !price || !images)
+  // throw new Error("something went wrong !");
   const createdPlace = await Place.create({
-   title,
-   description,
-   location,
-   host,
-   price,
-   images,
-   category,
-   from: new Date(from),
-   to: new Date(to),
-   maxAdults,
-   maxChildren,
-   maxInfants,
-   maxPets
-  })
+    title,
+    description,
+    location,
+    host,
+    price,
+    images,
+    category,
+    from: new Date(from),
+    to: new Date(to),
+    maxAdults,
+    maxChildren,
+    maxInfants,
+    maxPets,
+  });
   res.status(200).json({
-   status: "success",
-   body: {
-    createdPlace
-   }
-  })
- } catch (err) {
-  console.log(err);
-  res.status(400).json({
-   status: "fail",
-   reason: "something went wrong"
-  })
- }
-}
+    status: "success",
+    body: {
+      createdPlace,
+    },
+  });
+});
 
 // const getPlacesByCategory = async (req, res) =>
 // {
@@ -147,37 +147,33 @@ const createPlace = async (req, res) =>
 //  })
 // }
 
-const getMonthlyPlan = async (req, res) =>
-{
- try {
-  const year = parseInt(req.params.year);
+const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = parseInt(req.params.year);
 
-  const plan = await Place.aggregate([
-   { $unwind: '$ratingsCount' }
-  ])
+    const plan = await Place.aggregate([{ $unwind: "$ratingsCount" }]);
 
-  res.status(200).json({
-   status: "success",
-   result: plan.length,
-   body: {
-    plan
-   }
-  })
-
- } catch (error) {
-  res.status(400).json({
-   status: "fail",
-   reason: "something went wrong"
-  })
- }
-}
+    res.status(200).json({
+      status: "success",
+      result: plan.length,
+      body: {
+        plan,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      reason: "something went wrong",
+    });
+  }
+};
 
 module.exports = {
- getAllPlaces,
- getOnePlace,
- createPlace,
- uploadPlaceImages,
- // getPlacesByCategory,
- // getTopFiveChaep,
- getMonthlyPlan,
-}
+  getAllPlaces,
+  getOnePlace,
+  createPlace,
+  uploadPlaceImages,
+  // getPlacesByCategory,
+  // getTopFiveChaep,
+  getMonthlyPlan,
+};
--- index.js
+++ index.js
@@ -5,4 +5,8 @@ dotenv.config();
 const port = process.env.PORT || 8000;
 connectDB();
 
-app.listen(port, () => console.log(`server running on port ${port}...`));
+app.listen(port, () =>
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode, on port ${port}...`
+  )
+);
